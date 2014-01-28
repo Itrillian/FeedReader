b980a85be376e959b64ef448d00af00ec9e3fe49
Itrillian <Itrillian@gmail.com>
2014-01-29 00:47:36 +0100

    namespace info for atom

---
+++ src/main/java/itrillian/feedreader/rss/atom/package-info.java
@@ -0,0 +1,10 @@
+@XmlSchema(
+    namespace = ATOM,
+    elementFormDefault = XmlNsForm.QUALIFIED)
+package itrillian.feedreader.rss.atom;
+
+import static itrillian.feedreader.rss.RssSchemaTypes.ATOM;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
