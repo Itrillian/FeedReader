eee6098775b4e114955f4d5b091e274bccf7df17
Itrillian <Itrillian@gmail.com>
2014-01-29 00:48:12 +0100

    parse atom using jaxb

--- src/main/java/itrillian/feedreader/rss/atom/RssAtomParser.java
+++ src/main/java/itrillian/feedreader/rss/atom/RssAtomParser.java
@@ -1,11 +1,41 @@
-package itrillian.feedreader.rss;
+package itrillian.feedreader.rss.atom;
 
+import itrillian.feedreader.rss.RssParser;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RssAtomParser implements RssParser {
 
+	private static final Logger LOGGER = LogManager.getLogger(RssAtomParser.class);
+	
 	@Override
-	public void parseFeed(File file) {
+	public AtomFeed parseFeed(File file) {
+		AtomFeed atom = null;
+		try {
+			JAXBContext jc = JAXBContext.newInstance(AtomFeed.class);
+	
+	        Unmarshaller unmarshaller = jc.createUnmarshaller();
+	        atom = (AtomFeed) unmarshaller.unmarshal(file);
+	
+	        if(LOGGER.isDebugEnabled()) {
+	        	ByteArrayOutputStream output = new ByteArrayOutputStream();
+	        	Marshaller marshaller = jc.createMarshaller();
+		        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+	        	marshaller.marshal(atom, output);
+	        	LOGGER.debug("\n" + output);
+	        }
+		} catch(JAXBException e) {
+			e.printStackTrace();
+		}
+		return atom;
 	}
-
 }
