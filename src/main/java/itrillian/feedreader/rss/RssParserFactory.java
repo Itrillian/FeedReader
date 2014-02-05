44746af0840817cc94942d03ceba89ba430a9021
Itrillian <Itrillian@gmail.com>
2014-02-05 23:33:28 +0100

    Test parser
    - Test values of the parsed file
    - Refactored some code

--- src/main/java/itrillian/feedreader/rss/RssParserFactory.java
+++ src/main/java/itrillian/feedreader/rss/RssParserFactory.java
@@ -5,9 +5,7 @@ import itrillian.feedreader.rss.atom.RssAtomParser;
 public class RssParserFactory {
 
 	public static RssParser getParserInstance(String schema) {
-		if(schema.equals(RssSchemaTypes.ATOM)) {
-			return new RssAtomParser();
-		}
+		if(schema.equals(RssSchemaTypes.ATOM)) return new RssAtomParser();
 		throw new RuntimeException("Couldn't select a parser for this feed");
 	}
 }
--- src/test/java/itrillian/feedreader/rss/RssParserTest.java
+++ src/test/java/itrillian/feedreader/rss/RssParserTest.java
@@ -1,10 +1,13 @@
 package itrillian.feedreader.rss;
 
-import static itrillian.feedreader.rss.RssParserFactory.getParserInstance;
-import static itrillian.feedreader.rss.RssUtil.getSchema;
+import static itrillian.feedreader.rss.RssUtil.parseFeedFromFile;
+import static org.junit.Assert.assertEquals;
+import itrillian.feedreader.rss.atom.AtomEntry;
+import itrillian.feedreader.rss.atom.AtomFeed;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -15,9 +18,25 @@ public class RssParserTest {
 
 	@Test
 	public void parseFeedTest() throws ParserConfigurationException, SAXException, IOException {
-		File file = new File("src/test/resources/itrillian/feedreader/rss/atom/RssAtomTest.xml");
-		String schema = getSchema(file);
-		RssParser parser = getParserInstance(schema);
-		parser.parseFeed(file);
+		AtomFeed atomFeed = (AtomFeed) parseFeedFromFile(new File("src/test/resources/itrillian/feedreader/rss/atom/RssAtomTest.xml"));
+		AtomEntry entry = atomFeed.getEntries().get(0);
+		
+		/*
+		 * Validate feed
+		 */
+		assertEquals("Example Feed", atomFeed.getTitle());
+		assertEquals("http://example.org/", atomFeed.getLink());
+		assertEquals(new Date(1071340202000l), atomFeed.getUpdated());
+		assertEquals("urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6", atomFeed.getId());
+		assertEquals("John Doe", atomFeed.getAuthor().getName());
+		
+		/*
+		 * Validate entry
+		 */
+		assertEquals("Atom-Powered Robots Run Amok", entry.getTitle());
+		assertEquals("http://example.org/2003/12/13/atom03", entry.getLink());
+		assertEquals(new Date(1071340202000l), entry.getUpdated());
+		assertEquals("urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a", entry.getId());
+		assertEquals("Some text.", entry.getSummary());
 	}
 }
