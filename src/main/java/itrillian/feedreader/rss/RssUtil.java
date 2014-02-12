7b2a0b4dd52bf44e34a63b25b2958542323364d5
Itrillian <Itrillian@gmail.com>
2014-02-12 22:12:44 +0100

    Add schema from url

--- src/main/java/itrillian/feedreader/rss/RssUtil.java
+++ src/main/java/itrillian/feedreader/rss/RssUtil.java
@@ -5,10 +5,13 @@ import static itrillian.feedreader.xml.XmlAttributes.SCHEMA;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class RssUtil {
@@ -20,4 +23,25 @@ public class RssUtil {
 	public static RssFeed parseFeedFromFile(File file) throws ParserConfigurationException, SAXException, IOException {
 		return getParserInstance(getSchema(file)).parseFeed(file);
 	}
+
+	public static String getSchema(URL url) throws ParseException {
+		try {
+			return getSchema(getDocumentBuilder().parse(url.openStream()));
+		} catch (SAXException | IOException e) {
+			throw new ParseException(e);
+		}
+	}
+
+	private static String getSchema(Document document) {
+		return document.getDocumentElement().getAttribute(SCHEMA);
+	}
+
+	private static DocumentBuilder getDocumentBuilder() throws ParseException {
+		try {
+			return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new ParseException(e);
+		}
+	}
+
 }
--- src/test/java/itrillian/feedreader/rss/RssUtilTest.java
+++ src/test/java/itrillian/feedreader/rss/RssUtilTest.java
@@ -5,18 +5,25 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
-
 import javax.xml.parsers.ParserConfigurationException;
+import java.net.URL;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
 public class RssUtilTest {
-	
+
 	@Test
 	public void getAtomSchema() throws ParserConfigurationException, SAXException, IOException {
-		File file = new File("src/test/resources/itrillian/feedreader/rss/atom/RssAtomTest.xml");
-		String schema = RssUtil.getSchema(file);
+		validate(RssUtil.getSchema(new File("src/test/resources/itrillian/feedreader/rss/atom/RssAtomTest.xml")));
+	}
+
+	@Test
+	public void getAtomSchemaFromUrl() throws ParserConfigurationException, SAXException, IOException, MalformedURLException {
+		validate(RssUtil.getSchema(new URL("https://github.com/ChristopheMees.atom")));
+	}
+
+	private void validate(String schema) {
 		assertEquals(ATOM, schema);
 	}
 }
